e38b3874fdde5fa576162e3cd218bbcd6f8e75c0
imamamo <malena.delrosario@student.ateneo.edu>
2024-03-15 14:02:59 +0800

    evert "added base template"
    This reverts commit 7caf7fdf80cb49ce15fa2950cebb7c2eb93aac5c.

--- ledger/views.py
+++ ledger/views.py
@@ -2,10 +2,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Recipe
-
 
 
 class RecipeListView(ListView):
